5f61b307fde0761abf8f97b07ad4008476ba0e82
William Boswell <doujones@Williams-MacBook-Air.local>
2021-07-07 15:42:22 -0400

    added form onsubmit, added hooks and json stringify

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
+import React, {useState} from 'react'
 import { useForm } from 'react-hook-form'
 import './App.css';
 
 function App() {
+   const { register, handleSubmit, errors } = useForm()
+   const [userInfo, setUserInfo] = useState()
+   const onSubmit = (data) => {
+    setUserInfo(data)
+    console.log(data);
+   }
+
   return (
     <div className="container">
-      <form>
+      <pre>
+          {JSON.stringify(userInfo, undefined, 2)}
+      </pre>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Register Form</h1>
           <div className="ui divider"></div>
             <div className="ui form">
               <div className="field">
                 <label>Username</label>
                  <input type="text" name="username" 
-                placeholder="Username" />
+                placeholder="Username"ref={register} 
+                />
             </div>
              <div className="field">
                 <label>Email</label>
                  <input type="email" name="email" 
-                placeholder="Email" />
+                placeholder="Email" 
+                ref={register} 
+                />
             </div>
          <div className="field">
                 <label>Password</label>
                  <input type="password" name="password" 
-                placeholder="Enter Password" />
+                placeholder="Enter Password" 
+                ref={register} 
+                />
             </div>
          </div>
             <button className="fluid ui button blue">Submit</button>
